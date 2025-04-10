2edb98687276bf1d6c6a58a7cf56d395319424ab
Mehloul Mohamed <m.mehloul@esi-sba.dz>
2025-04-10 18:20:23 +0100

    Feat: minor tweaks

--- Backend/urls.py
+++ Backend/urls.py
@@ -22,10 +22,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/users/',include('users.urls')),
+    path('api/v1/auth/',include('users.urls')),
     path('api/v1/labs/',include('labs.urls')),
     path('api/v1/ranking/',include('ranking.urls')),
-
     path('api/v1/courses/',include('courses.urls')),
 
     # Swagger stuff for docs (TODO: comment out in prod)
