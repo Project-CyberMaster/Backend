0d28df44649bf16f680eeea0b114d2e5775816da
Mehloul Mohamed <m.mehloul@esi-sba.dz>
2025-04-16 20:26:57 +0100

    Unfuck everything

--- labs/urls.py
+++ labs/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import  *
 
 urlpatterns = [
-    path('', LabListCreate.as_view(), name='lab-list-create'),
+    path('', LabList.as_view(), name='lab-list-create'),
     path('<int:pk>', LabDetail.as_view(), name='lab-detail'),
     path('search', Search.as_view(), name='lab-search'),
    
