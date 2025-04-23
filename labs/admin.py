2b53b0ab25e2a1c6744f7dab90c90be3161677c6
Mehloul Mohamed <m.mehloul@esi-sba.dz>
2025-04-23 02:56:10 +0100

    jiojefe;jioawefjo wooooooooooooo^2

--- labs/admin.py
+++ labs/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import *
 
-
 admin.site.register(Lab)
+admin.site.register(Machine)
 admin.site.register(LabResourceFile)
 admin.site.register(SolvedLab)
 admin.site.register(Badge)
